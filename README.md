172333b4121d8e9d0f7468d97560925869497956
Pierre Carbonnelle <pierre.carbonnelle@gmail.com>
2026-04-28 15:10:36 +0200

    1st run

---
+++ README.md
@@ -0,0 +1,1 @@
+Run the benchmark using `uv run main.py`.
--- src/run.py
+++ src/run.py
@@ -1,24 +1,33 @@
+import os
 import time
 import subprocess
 import tempfile
 import resource
+import csv
+from datetime import datetime
 
 print(subprocess.check_output(["z3", "--version"], text=True))
 
-timeout = 60
+if not os.path.exists('result.csv'):
+    with open('result.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['name', 'solver', 'solve time', 'error', 'run date'])
+
+timeout = 300
 GB = 1024 * 1024 * 1024
-MEMORY_LIMIT = 4 * GB
+MEMORY_LIMIT = 8 * GB
 
 def limit_memory():
     resource.setrlimit(resource.RLIMIT_AS, (MEMORY_LIMIT, MEMORY_LIMIT))
 
-def run_xmt(script):
+def run_xmt(script, name, result):
     print("running xmt")
     with tempfile.NamedTemporaryFile("w") as file:
         file.write(script)
         file.flush()
 
         start_time = time.time()
+        error = ""
 
         try:
             # call xmt-lib with a timeout
@@ -29,24 +38,51 @@ def run_xmt(script):
                 timeout=timeout,
                 preexec_fn=limit_memory
             )
-            result = process.stdout
+            stdout = process.stdout
             error = process.stderr
 
-            end_time = time.time()
-
-            if result:
-                print(f"Result:\n{result.strip()}")
+            if stdout:
+                print(f"Result:\n{stdout.strip()}")
+                if not result in stdout:
+                    print(f"*******  Incorrect result  !!!!!!!!!!")
+                    error = f"Incorrect: {stdout}"
             if error:
                 print(f"Error output:\n{error.strip()}")
-            print(f"Total time taken: {end_time - start_time:.4f} seconds")
+                error = f"stderr : {error.strip()}"
 
         except subprocess.TimeoutExpired:
             print(f"Error: xmt-lib execution timed out after {timeout} seconds.")
+            error = "Time out"
         except Exception as e:
             print(f"Error evaluating SMT-LIB: {e}")
+            error = str(e)
 
-def run_z3(script):
+    end_time = f"{time.time() - start_time:.4f}"
+    print(f"Total time taken: {end_time} seconds")
+
+    with open('result.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([name, "xmt", end_time, error or "", datetime.now().isoformat()])
+
+
+def run_z3(script, logic, name, result):
     print("running z3")
+
+    output_dir = os.path.join(
+        "..",
+        "benchmark-submission",
+        "non-incremental",
+        logic,
+        "2026-05-28-Grounders"
+    )
+    os.makedirs(output_dir, exist_ok=True)
+
+    path = os.path.join(output_dir, f"{name}.smt")
+    with open(path, "w", encoding="utf-8") as file:
+        file.write(script)
+
+    print(f"Saved SMT script to: {path}")
+
     start_time = time.time()
 
     try:
@@ -59,23 +95,33 @@ def run_z3(script):
             timeout=timeout,
             preexec_fn=limit_memory
         )
-        result = process.stdout
+        stdout = process.stdout
         error = process.stderr
 
-        end_time = time.time()
-
-        if result:
-            print(f"Result:\n{result.strip()}")
+        if stdout:
+            print(f"Result:\n{stdout.strip()}")
+            if not result in stdout:
+                print(f"*******  Incorrect result  !!!!!!!!!!")
+                error = f"Incorrect: {stdout}"
         if error:
             print(f"Error output:\n{error.strip()}")
-        print(f"Total time taken: {end_time - start_time:.4f} seconds")
+            error = f"stderr : {error.strip()}"
 
     except subprocess.TimeoutExpired:
-        print(f"Error: Z3 execution timed out after {timeout} seconds.")
+        print(f"Error: xmt-lib execution timed out after {timeout} seconds.")
+        error = "Time out"
     except Exception as e:
         print(f"Error evaluating SMT-LIB: {e}")
+        error = str(e)
 
-def run_cvc5(script):
+    end_time = f"{time.time() - start_time:.4f}"
+    print(f"Total time taken: {end_time} seconds")
+
+    with open('result.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([name, "z3", end_time, error or "", datetime.now().isoformat()])
+
+def run_cvc5(script, name, result):
     print("running cvc5")
     start_time = time.time()
 
@@ -89,18 +135,28 @@ def run_cvc5(script):
             timeout=timeout,
             preexec_fn=limit_memory
         )
-        result = process.stdout
+        stdout = process.stdout
         error = process.stderr
 
-        end_time = time.time()
-
-        if result:
-            print(f"Result:\n{result.strip()}")
+        if stdout:
+            print(f"Result:\n{stdout.strip()}")
+            if not result in stdout:
+                print(f"*******  Incorrect result  !!!!!!!!!!")
+                error = f"Incorrect: {stdout}"
         if error:
             print(f"Error output:\n{error.strip()}")
-        print(f"Total time taken: {end_time - start_time:.4f} seconds")
+            error = f"stderr : {error.strip()}"
 
     except subprocess.TimeoutExpired:
-        print(f"Error: cvc5 execution timed out after {timeout} seconds.")
+        print(f"Error: xmt-lib execution timed out after {timeout} seconds.")
+        error = "Time out"
     except Exception as e:
         print(f"Error evaluating SMT-LIB: {e}")
+        error = str(e)
+
+    end_time = f"{time.time() - start_time:.4f}"
+    print(f"Total time taken: {end_time} seconds")
+
+    with open('result.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([name, "cvc5", end_time, error or "", datetime.now().isoformat()])
